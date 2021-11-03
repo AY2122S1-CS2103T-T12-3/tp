51e76310a688f1af70bf237351c59825f7a04308
tchiong <57283702+tchiong@users.noreply.github.com>
2021-11-03 22:25:32 +0800

    Fix Test Error

--- src/test/java/seedu/placebook/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/placebook/storage/JsonUserPrefsStorageTest.java
@@ -73,7 +73,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setContactsFilePath(Paths.get("addressbook.json"));
+        userPrefs.setContactsFilePath(Paths.get("data/contacts.json"));
         return userPrefs;
     }
 
