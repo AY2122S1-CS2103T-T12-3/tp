f4b02af9efdf90369f656337da4f84f5bdafd9ee
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-11-04 18:11:33 +0800

    Update JsonScheduleStorageTest

--- src/test/java/seedu/placebook/storage/JsonScheduleStorageTest.java
+++ src/test/java/seedu/placebook/storage/JsonScheduleStorageTest.java
@@ -1,8 +1,13 @@
 package seedu.placebook.storage;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.placebook.testutil.Assert.assertThrows;
+import static seedu.placebook.testutil.TypicalAppointment.ALICE_APPOINTMENT;
+import static seedu.placebook.testutil.TypicalAppointment.ALICE_CARL_APPOINTMENT;
+import static seedu.placebook.testutil.TypicalAppointment.getTypicalSchedule;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -11,6 +16,7 @@ import org.junit.jupiter.api.io.TempDir;
 
 import seedu.placebook.commons.exceptions.DataConversionException;
 import seedu.placebook.model.ReadOnlySchedule;
+import seedu.placebook.model.schedule.Schedule;
 
 public class JsonScheduleStorageTest {
     private static final Path TEST_DATA_FOLDER =
@@ -45,12 +51,61 @@ public class JsonScheduleStorageTest {
     }
 
     @Test
-    public void readSchedule_invalidPersonContacts_throwDataConversionException() {
-        assertThrows(DataConversionException.class, () -> readSchedule("invalidPersonSchedule.json"));
+    public void readSchedule_invalidAppointmentSchedule_throwDataConversionException() {
+        assertThrows(DataConversionException.class, () -> readSchedule("invalidAppointmentSchedule.json"));
     }
 
     @Test
-    public void readSchedule_invalidAndValidPersonContacts_throwDataConversionException() {
-        assertThrows(DataConversionException.class, () -> readSchedule("invalidAndValidPersonSchedule.json"));
+    public void readSchedule_invalidAndValidAppointmentSchedule_throwDataConversionException() {
+        assertThrows(DataConversionException.class, ()
+            -> readSchedule("invalidAndValidAppointmentSchedule.json"));
+    }
+
+    @Test
+    public void readAndSaveSchedule_allInOrder_success() throws Exception {
+        Path filePath = testFolder.resolve("TempContacts.json");
+        Schedule original = getTypicalSchedule();
+        JsonScheduleStorage jsonScheduleStorage = new JsonScheduleStorage(filePath);
+
+        // Save in new file and read back
+        jsonScheduleStorage.saveSchedule(original, filePath);
+        ReadOnlySchedule readBack = jsonScheduleStorage.readSchedule(filePath).get();
+        assertEquals(original, new Schedule(readBack));
+
+        // Modify data, overwrite exiting file, and read back
+        original.addAppointment(ALICE_CARL_APPOINTMENT);
+        original.deleteAppointment(ALICE_APPOINTMENT);
+        jsonScheduleStorage.saveSchedule(original, filePath);
+        readBack = jsonScheduleStorage.readSchedule(filePath).get();
+        assertEquals(original, new Schedule(readBack));
+
+        // Save and read without specifying file path
+        original.addAppointment(ALICE_APPOINTMENT);
+        jsonScheduleStorage.saveSchedule(original); // file path not specified
+        readBack = jsonScheduleStorage.readSchedule().get(); // file path not specified
+        assertEquals(original, new Schedule(readBack));
+
+    }
+
+    @Test
+    public void saveSchedule_nullSchedule_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> saveSchedule(null, "SomeFile.json"));
+    }
+
+    /**
+     * Saves {@code contacts} at the specified {@code filePath}.
+     */
+    private void saveSchedule(ReadOnlySchedule schedule, String filePath) {
+        try {
+            new JsonScheduleStorage(Paths.get(filePath))
+                    .saveSchedule(schedule, addToTestDataPathIfNotNull(filePath));
+        } catch (IOException ioe) {
+            throw new AssertionError("There should not be an error writing to the file.", ioe);
+        }
+    }
+
+    @Test
+    public void saveSchedule_nullFilePath_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> saveSchedule(new Schedule(), null));
     }
 }
