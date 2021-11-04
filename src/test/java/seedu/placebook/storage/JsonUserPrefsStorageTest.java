9d209040de09ddbc89663036046a6b01521bea10
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-11-04 19:24:17 +0800

    Fix checkstyle

--- src/test/java/seedu/placebook/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/placebook/storage/JsonUserPrefsStorageTest.java
@@ -1,8 +1,8 @@
 package seedu.placebook.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static seedu.placebook.testutil.Assert.assertThrows;
 
 import java.io.IOException;
