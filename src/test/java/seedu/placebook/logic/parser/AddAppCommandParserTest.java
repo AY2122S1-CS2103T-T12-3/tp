40a1911fc7b185bb3809b3e6c5126240eedb3c0e
tchiong <57283702+tchiong@users.noreply.github.com>
2021-11-04 22:15:25 +0800

    Update AddAppCommandParserTest.java

--- src/test/java/seedu/placebook/logic/parser/AddAppCommandParserTest.java
+++ src/test/java/seedu/placebook/logic/parser/AddAppCommandParserTest.java
@@ -61,23 +61,23 @@ public class AddAppCommandParserTest {
 
         // missing index
         assertParseFailure(parser,
-                "id/ a/vivocity start/01-01-2021 1000 end/01-01-2021 1200 ds/",
+                " id/ a/vivocity start/01-01-2021 1000 end/01-01-2021 1200 ds/Halloween Sales",
                 "Index is not a non-zero unsigned integer.");
 
         // missing address
         assertParseFailure(parser,
-                " id/1 a/ start/01-01-2021 1000 end/01-01-2021 1200 ds/",
+                " id/1 a/ start/01-01-2021 1000 end/01-01-2021 1200 ds/Halloween Sales",
                 "Addresses can take any values, and it should not be blank");
 
         // missing start
         assertParseFailure(parser,
-                " id/1 a/vivocity start/ end/01-01-2021 1000 ds/",
+                " id/1 a/vivocity start/ end/01-01-2021 1000 ds/Halloween Sales",
                 "DateTime format should be \"dd-MM-yyyy HHmm\"."
                         + " Don't forget that there are only 12 months in a year!");
 
         // missing end
         assertParseFailure(parser,
-                " id/1 a/vivocity start/01-01-2021 1000 end/ ds/",
+                " id/1 a/vivocity start/01-01-2021 1000 end/ ds/Halloween Sales",
                 "DateTime format should be \"dd-MM-yyyy HHmm\"."
                         + " Don't forget that there are only 12 months in a year!");
     }
@@ -87,24 +87,24 @@ public class AddAppCommandParserTest {
 
         // invalid id
         assertParseFailure(parser,
-                "id/at a/vivocity start/01-01-2021 1000 end/01-01-2021 1200 ds/",
+                " id/at a/vivocity start/01-01-2021 1000 end/01-01-2021 1200 ds/Halloween Sales",
                 "Index is not a non-zero unsigned integer.");
 
         // invalid start, invalid values
         assertParseFailure(parser,
-                " id/1 a/vivocity start/00-00-0000 0000 end/01-01-2021 1000 ds/",
+                " id/1 a/vivocity start/00-00-0000 0000 end/01-01-2021 1000 ds/Halloween Sales",
                 "DateTime format should be \"dd-MM-yyyy HHmm\"."
                         + " Don't forget that there are only 12 months in a year!");
 
         // invalid start, missing values
         assertParseFailure(parser,
-                " id/1 a/vivocity start/01-01-2021 end/01-01-2021 1000 ds/",
+                " id/1 a/vivocity start/01-01-2021 end/01-01-2021 1000 ds/Halloween Sales",
                 "DateTime format should be \"dd-MM-yyyy HHmm\"."
                         + " Don't forget that there are only 12 months in a year!");
 
         // invalid end, invalid values
         assertParseFailure(parser,
-                " id/1 a/vivocity start/01-01-2021 1000 end/00-00-0000 0000 ds/",
+                " id/1 a/vivocity start/01-01-2021 1000 end/00-00-0000 0000 ds/Halloween Sales",
                 "DateTime format should be \"dd-MM-yyyy HHmm\"."
                         + " Don't forget that there are only 12 months in a year!");
 
