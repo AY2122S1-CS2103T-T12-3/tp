33ca54bd3aac29c20bc1b43d984879f71fce19bd
YaleChen299 <proverb299@gmail.com>
2021-11-04 10:30:09 +0800

    remove address book

--- src/main/java/seedu/placebook/logic/commands/AddCommand.java
+++ src/main/java/seedu/placebook/logic/commands/AddCommand.java
@@ -35,7 +35,7 @@ public class AddCommand extends Command {
             + PREFIX_TAG + "owesMoney";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the Contacts";
 
     private final Person toAdd;
 
--- src/test/java/seedu/placebook/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/placebook/logic/commands/EditCommandTest.java
@@ -153,7 +153,7 @@ public class EditCommandTest {
     public void execute_duplicatePersonFilteredList_failure() {
         showPersonAtIndex(model, INDEX_FIRST_PERSON);
 
-        // edit person in filtered list into a duplicate in address book
+        // edit person in filtered list into a duplicate in contacts
         Person personInList = model.getContacts().getPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
         EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON,
                 new EditPersonDescriptorBuilder(personInList).build());
@@ -172,13 +172,13 @@ public class EditCommandTest {
 
     /**
      * Edit filtered list where index is larger than size of filtered list,
-     * but smaller than size of address book
+     * but smaller than size of contacts
      */
     @Test
     public void execute_invalidPersonIndexFilteredList_failure() {
         showPersonAtIndex(model, INDEX_FIRST_PERSON);
         Index outOfBoundIndex = INDEX_SECOND_PERSON;
-        // ensures that outOfBoundIndex is still in bounds of address book list
+        // ensures that outOfBoundIndex is still in bounds of contacts list
         assertTrue(outOfBoundIndex.getZeroBased() < model.getContacts().getPersonList().size());
 
         EditCommand editCommand = new EditCommand(outOfBoundIndex,
