708bee538ff6eb77681e292ab178d2e703cc5fe7
Yew Meng <yewmengchai@gmail.com>
2021-11-04 22:12:48 +0800

    Fix checkstyle

--- src/test/java/seedu/placebook/logic/parser/FindTagsCommandParserTest.java
+++ src/test/java/seedu/placebook/logic/parser/FindTagsCommandParserTest.java
@@ -17,7 +17,8 @@ public class FindTagsCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTagsCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTagsCommand.MESSAGE_USAGE));
     }
 
     @Test
