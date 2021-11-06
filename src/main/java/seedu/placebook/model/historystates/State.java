356d1b47950e4938282699ddc0b8872eba37beea
Li-Lehao <e0492462@u.nus.edu>
2021-11-06 11:57:12 +0800

    Lehao - Undo command bug fixes
    The previous undo has such a bug: when delete person and then undo, the client list of the affected appointment will not be refreshed (undo). This is because a state simply copy the reference of the schedule, and different schedules in different states actually refer to the same appointment and same client list. To solve this we need to create deep copy when initialize the state and get schedule of the state.

--- src/main/java/seedu/placebook/model/historystates/State.java
+++ src/main/java/seedu/placebook/model/historystates/State.java
@@ -1,6 +1,11 @@
 package seedu.placebook.model.historystates;
 
+import java.util.List;
+
 import seedu.placebook.model.Contacts;
+import seedu.placebook.model.person.Person;
+import seedu.placebook.model.person.UniquePersonList;
+import seedu.placebook.model.schedule.Appointment;
 import seedu.placebook.model.schedule.Schedule;
 
 /**
@@ -20,23 +25,90 @@ public class State {
      * @param schedule The given {@Code Schedule}.
      */
     public State(Contacts contacts, Schedule schedule) {
-        this.contacts = new Contacts(contacts);
-        this.schedule = new Schedule(schedule);
+        this.contacts = new Contacts();
+        List<Person> personList = contacts.getPersonList();
+        for (Person personToCopy : personList) {
+            this.contacts.addPerson(
+                    new Person(
+                            personToCopy.getName(),
+                            personToCopy.getPhone(),
+                            personToCopy.getEmail(),
+                            personToCopy.getAddress(),
+                            personToCopy.getTags()
+                    ));
+        }
+
+        this.schedule = new Schedule();
+        List<Appointment> appointmentList = schedule.getSchedule();
+        for (Appointment appointmentToCopy : appointmentList) {
+            UniquePersonList clientList = new UniquePersonList();
+            for (Person clientToCopy : appointmentToCopy.getClients()) {
+                clientList.add(
+                        new Person(
+                                clientToCopy.getName(),
+                                clientToCopy.getPhone(),
+                                clientToCopy.getEmail(),
+                                clientToCopy.getAddress(),
+                                clientToCopy.getTags()
+                        ));
+            }
+            this.schedule.addAppointment(
+                    new Appointment(
+                            clientList,
+                            appointmentToCopy.getLocation(),
+                            appointmentToCopy.getTimePeriod(),
+                            appointmentToCopy.getDescription()
+                    ));
+        }
     }
 
     /**
      * Get the {@Code Contacts} of this state.
-     * @return A copy of the {@Code Contacts} of this state.
+     * @return A deep copy of the {@Code Contacts} of this state.
      */
     public Contacts getContacts() {
-        return new Contacts(this.contacts);
+        Contacts result = new Contacts();
+        for (Person personToCopy : this.contacts.getPersonList()) {
+            result.addPerson(
+                    new Person(
+                            personToCopy.getName(),
+                            personToCopy.getPhone(),
+                            personToCopy.getEmail(),
+                            personToCopy.getAddress(),
+                            personToCopy.getTags()
+                    )
+            );
+        }
+        return result;
     }
 
     /**
      * Get the {@Code Schedule} of this state.
-     * @return A copy of the {@Code Schedule} of this state.
+     * @return A deep copy of the {@Code Schedule} of this state.
      */
     public Schedule getSchedule() {
-        return new Schedule(this.schedule);
+        Schedule result = new Schedule();
+        List<Appointment> appointmentList = schedule.getSchedule();
+        for (Appointment appointmentToCopy : appointmentList) {
+            UniquePersonList clientList = new UniquePersonList();
+            for (Person clientToCopy : appointmentToCopy.getClients()) {
+                clientList.add(
+                        new Person(
+                                clientToCopy.getName(),
+                                clientToCopy.getPhone(),
+                                clientToCopy.getEmail(),
+                                clientToCopy.getAddress(),
+                                clientToCopy.getTags()
+                        ));
+            }
+            result.addAppointment(
+                    new Appointment(
+                            clientList,
+                            appointmentToCopy.getLocation(),
+                            appointmentToCopy.getTimePeriod(),
+                            appointmentToCopy.getDescription()
+                    ));
+        }
+        return result;
     }
 }
