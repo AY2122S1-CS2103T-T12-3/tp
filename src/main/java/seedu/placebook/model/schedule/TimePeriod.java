b9aa1e9a6110b0e5abc0d9bd4e0cea49ff30c529
YaleChen299 <proverb299@gmail.com>
2021-11-06 19:07:13 +0800

    remove unused import

--- src/main/java/seedu/placebook/model/schedule/TimePeriod.java
+++ src/main/java/seedu/placebook/model/schedule/TimePeriod.java
@@ -1,8 +1,6 @@
 package seedu.placebook.model.schedule;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
